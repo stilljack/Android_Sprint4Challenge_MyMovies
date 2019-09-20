ad18f1cf0806599574cbf1a5c227ef0fe18dd8ec
jack seymour <stilljack@gmail.com>
2019-09-20 09:08:10 -0700

    sprint 7, jack seymour 9/20/2019 -- 9:07PST
    windows update can F right off

--- app/src/main/java/com/lambdaschool/datapersistencesprintchallenge/MainActivity.kt
+++ app/src/main/java/com/lambdaschool/datapersistencesprintchallenge/MainActivity.kt
@@ -3,6 +3,8 @@ package com.lambdaschool.datapersistencesprintchallenge
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/// init: yikes first line written an hour late...
+/// almost like something went wrong....  hahah here we go
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
